96736e9475fe719391c3bb45dbbb4fbb0a86ed80
Tania Filonenko <tetiana.filonenko2012@gmail.com>
2026-05-09 21:18:28 +0100

    Add lab 7

---
+++ terraform/argocd/main.tf
@@ -0,0 +1,30 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = var.argocd_namespace
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = var.argocd_chart_version
+
+  values = [
+    file("${path.module}/values/argocd-values.yaml")
+  ]
+
+  timeout = 600
+}
---
+++ terraform/argocd/variables.tf
@@ -0,0 +1,7 @@
+variable "argocd_namespace" {
+  default = "infra-tools"
+}
+
+variable "argocd_chart_version" {
+  default = "5.51.6"
+}
